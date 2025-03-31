85bc2d90becdb7e56e8a3316b031f8efead68103
Cintia-97 <cintia.gonsalez@outlook.com.br>
2025-03-30 21:02:00 -0300

    Create

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import Express from 'express';
+
+// Initiate express
+const app = Express();
+
+app.use(Express.json());
+
+// Setup “hello world” endpoint
+
+const port = process.env.PORT || 3000;
+
+app.get('*', (req: any, res: any) => res.send('Hello World!'));
+
+// Start the express server on the relevant port
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
